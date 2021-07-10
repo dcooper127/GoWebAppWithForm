ccb3126e4ab00c5c8d95642f2b989dcf5f3a6ca0
dcooper127 <dcooper127@gmail.com>
2021-07-10 18:36:56 -0400

    Initial commit

---
+++ form.gtpl
@@ -0,0 +1,37 @@
+<html>
+    <head>
+        <style>
+            div.myText{max-width:40%;}
+            body{background-color: #ddf8ff;}
+        </style>
+
+        <title> Go Anagram Webpage </title>
+    </head>
+
+    <center> </center><h1> David Cooper's Webpage for Go Anagram Solutions </h1> </center>
+
+    <hr>
+
+    <div class ="myText">
+        <h2>Instructions</h2>
+
+        <p>
+            Enter one word into the textbox for "Word 1" and the second into the textbox for "Word 2" and click "Submit" to see if the words are anagrams of each other. Note: is not case sensitive.
+        </p>
+
+    </div>
+
+    <hr>
+
+    <body>
+        <form method="post">
+            Word 1:<input type="text" name="ip1">
+            Word 2:<input type="text" name="ip2">
+            <input type="submit" value="Submit">
+        </form>
+        Result: {{.Input1}} and {{.Input2}} {{.Result}}
+    </body>
+
+    <div class ="myText"> Github source code: https://github.com/dcooper127/GoPrograms</div>
+
+</html>
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
+)
+
+type myValues struct{
+	Input1 string
+	Input2 string
+	Result string
+}
+
+//Uses mapping for an 0(n) solution to see if two words are anagrams of each other.
+//Strategy: uses (character, occurrence) mapping to count the occurrence of each character in the first word and subtracts
+//the occurrence of each character in the second word. If any character in the map has a non 0 value then
+//it appeared in one word and not the other, therefore the words are not anagrams of each other.
+func isAnagram(word1 string, word2 string) bool {
+	var letterCounts map[string]int = make(map[string]int)
+
+	//lowercase both words
+	word1 = strings.ToLower(word1)
+	word2 = strings.ToLower(word2)
+
+	//
+
+
+	//itterate over word 1
+	for _,char := range word1 {
+		charCount, present := letterCounts[string(char)]
+		if !(present) {
+			letterCounts[string(char)] = 1
+		} else {
+			letterCounts[string(char)] = charCount + 1
+		}
+	}
+
+	for _,char := range word2 {
+		charCount, present := letterCounts[string(char)]
+		if !(present) {
+			letterCounts[string(char)] = -1
+		} else {
+			letterCounts[string(char)] = charCount - 1
+		}
+	}
+
+	//itterate over word 2
+	for _,v := range letterCounts {
+		if(v != 0) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func testFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("form.gtpl")
+		t.Execute(w, "")
+	} else {
+		r.ParseForm()
+		output := isAnagram(r.Form["ip1"][0] ,r.Form["ip2"][0])
+
+		outputString := ""
+
+		if output {
+			outputString = "are anagrams."
+		} else {
+			outputString = "are not anagrams."
+		}
+
+		values := myValues{Input1: r.Form["ip1"][0] ,Input2: r.Form["ip2"][0], Result: outputString}
+
+		t, _ := template.ParseFiles("form.gtpl")
+		t.Execute(w, values)
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/",testFunc)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
